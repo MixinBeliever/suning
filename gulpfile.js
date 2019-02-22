959e20a600a45a6951855ab6270554d5bf38068b
liupan <863420364@qq.com>
2019-02-22 08:40:12 +0800

    增加了轮播图

--- gulpfile.js
+++ gulpfile.js
@@ -4,4 +4,32 @@ var uglify = require('gulp-uglify');
 var spritesmith = require('gulp.spritesmith');  
 var gulp_sass = require('gulp-sass');
 
-//555
+//// Gulp要做的事情：
+// 1.	配置gulp环境
+// 2.	拷贝index
+// 3.	拷贝html下的所有*.html
+// 4. 	拷贝css
+// 5.	连接、压缩、生成output.js
+// 6.   拷贝imgs下的所有图片
+// 7.	生成精灵图
+// 8.	拷贝库文件
+// 9.	监听src下文件的改变，自动进行构建
+// 10.	自动刷新页面
+
+//拷贝index
+function index(){
+    return gulp.src('./src/index.html')
+                .pipe(gulp.dest('./dist'))
+}
+gulp.task('index',index)
+//拷贝html下的所有*.html
+
+
+
+
+
+
+function watch(){
+     gulp.watch('./src/index.html',index)
+}
+gulp.task('watch',watch)
